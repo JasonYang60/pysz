f34fcd092d16df4816253ded6f8fe4a0634f5ce8
NeptuneYang <53274224+NeptuneYang@users.noreply.github.com>
2024-07-01 00:30:21 +0800

    + readfile() & writefile()

--- pysz/compress.pxd
+++ pysz/compress.pxd
@@ -1,10 +1,45 @@
+from libcpp.string cimport string
+from libcpp cimport bool
+from libc.stdint cimport uint8_t
+from libcpp.vector cimport vector
+from libcpp.memory cimport unique_ptr
+
 cdef extern from "SZ3/utils/Config.hpp" namespace "SZ3":
     cdef cppclass Config:
-        pass
+        size_t setDims[Iter](Iter begin, Iter end)
+        void loadcfg(const string &cfgpath) 
+        char N
+        vector[size_t] dims
+        size_t num 
+        uint8_t cmprAlgo
+        uint8_t errorBoundMode 
+        double absErrorBound 
+        double relErrorBound 
+        double psnrErrorBound 
+        double l2normErrorBound 
+        bool lorenzo 
+        bool lorenzo2 
+        bool regression 
+        bool regression2 
+        bool openmp 
+        uint8_t lossless # 0-> skip lossless(use lossless_bypass); 1-> zstd
+        uint8_t encoder # 0-> skip encoder; 1->HuffmanEncoder; 2->ArithmeticEncoder
+        uint8_t interpAlgo 
+        uint8_t interpDirection 
+        int interpBlockSize
+        int quantbinCnt
+        int blockSize
+        int stride #not used now
+        int pred_dim # not used now
 
 cdef extern from "SZ3/api/sz.hpp":
     char *SZ_compress[T](const Config &conf, 
                 const T *data, size_t &cmpSize)
+
+cdef extern from "SZ3/utils/FileUtil.hpp" namespace "SZ3":
+    void readfile[Type](const char *file, const size_t num, Type *data)
+    #unique_ptr[Type[]] readfile[Type](const char *file, size_t &num)
+    void writefile[Type](const char *file, Type *data, size_t num_elements)
 
 # Define the macros as constants in Cython
 cdef int SZ_FLOAT   = 0
--- pysz/compress.pyx
+++ pysz/compress.pyx
@@ -1,27 +1,40 @@
+# distutils: language = c++
 # Import the interface definitions
 from pysz cimport compress
 cimport cython
 
-
 def compress_data(str inPath):
                 #const char *cmpPath, 
                 #cmp.SZ3.Config conf):
-    cdef double data[8192] # conf.num = 8192
+    #cdef double data[8192] # conf.num = 8192
 #    cdef char *result = SZ_compress(conf, &data[0], cmpSize)
-    inBytes = ""
-    with open(inPath, 'rb') as f:
-        inBytes = f.read()
+    #inStr = ""
+    #with open(inPath, 'rb') as f:
+    #    inStr = f.read()
+    #cdef char *inBytes = nullptr
+    cdef double *inBytes;
+    cdef char *inPathBytes = <bytes>inPath
+    readfile[double](inPathBytes, 8192, inBytes)
 
-    cdef char *outBytes = inBytes
-    #cdef char *outBytes = SZ_compress(conf, )
+    cdef Config conf
+    conf.loadcfg("sz.config")
+    conf.blockSize = 6;
+    cdef vector[size_t] dims = {128, 8, 8}
+    conf.setDims(dims.begin(), dims.end())
+    cdef double *outBytes = inBytes
+    cdef size_t outSize = 8192
+    #cdef char *outBytes = SZ_compress[double](conf, inBytes, outSize)
 
     #if (cmpPath == nullptr):
     #    outputFilePath += "%s.sz" + inPath
     #else:
     outputFilePath = inPath + ".sz"
-    py_outBytes = <bytes> outBytes
-    with open(outputFilePath, 'wb') as f:
-        f.write(py_outBytes)
+    cdef char* outPathBytes = outputFilePath
+    writefile[double](outPathBytes, outBytes, outSize)
+    #cdef char *outCharPtr = <char*>outBytes
+    #py_outBytes = <bytes> outCharPtr
+    #with open(outputFilePath, 'wb') as f:
+    #    f.write(py_outBytes)
     
 #    write_char_array_to_file(data, 8192, outputFilePath)
 
